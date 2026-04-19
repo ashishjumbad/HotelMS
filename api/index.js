195c45d2d6151949ce1cfcff2f107a527de5edc9
ashish <ashishjumbad005@gmail.com>
2026-04-20 00:31:02 +0530

    your message

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import app from "../backend/app.js";
 
 export default function handler(req, res) {
-  return app(req, res);
+  try {
+    app(req, res);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
 }
